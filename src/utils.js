41e13c3e74babb7e4b779a738f94e1238b69f944
IlyaAgarishev <ilyagarishev@gmail.com>
2019-04-22 02:34:44 +0500

    wordEndingGenerator logic updated

--- src/utils.js
+++ src/utils.js
@@ -3,14 +3,14 @@
 export const wordEndingGenerator = (wordsLimit, dictionaryLength) => {
   let wordsLeft = wordsLimit - dictionaryLength;
   if (
-    (wordsLeft >= 5 && wordsLeft <= 14) ||
     (wordsLeft % 10 >= 5 && wordsLeft % 10 <= 9) ||
-    wordsLeft % 10 === 0
+    wordsLeft % 10 === 0 ||
+    (wordsLeft >= 11 && wordsLeft <= 14)
   ) {
     return `${wordsLeft} слов`;
-  } else if (wordsLeft >= 2 && wordsLeft <= 4) {
+  } else if (wordsLeft % 10 >= 2 && wordsLeft % 10 <= 4) {
     return `${wordsLeft} слова`;
-  } else if (wordsLeft === 1 || (wordsLeft % 10 >= 1 && wordsLeft % 10 <= 4)) {
+  } else if (wordsLeft % 10 === 1 && wordsLeft !== 11) {
     return `${wordsLeft} слово`;
   }
 };
