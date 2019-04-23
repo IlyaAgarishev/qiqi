62f4a4bf4bec24ac6d1e0e805968c8db5ed9cd88
IlyaAgarishev <ilyagarishev@gmail.com>
2019-04-23 19:03:25 +0500

    eslintrc

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -16,20 +16,6 @@ const generatedWord = (wordsLimit, dictionaryLength) => {
   return wordEndingGenerator(wordsLimit, dictionaryLength).split(" ")[1];
 };
 
-test("wordEndingGenerator returns 'слово'", () => {
+test("wordEndingGenerator returns right word", () => {
   expect(generatedWord(30, 9)).toBe("слово");
-});
-
-test("wordEndingGenerator returns 'слова'", () => {
-  expect(generatedWord(30, 8)).toBe("слова");
-  expect(generatedWord(30, 7)).toBe("слова");
-  expect(generatedWord(30, 6)).toBe("слова");
-});
-
-test("wordEndingGenerator returns 'слов'", () => {
-  expect(generatedWord(30, 5)).toBe("слов");
-  expect(generatedWord(30, 4)).toBe("слов");
-  expect(generatedWord(30, 3)).toBe("слов");
-  expect(generatedWord(30, 2)).toBe("слов");
-  expect(generatedWord(30, 1)).toBe("слов");
 });
